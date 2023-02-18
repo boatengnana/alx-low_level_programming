af8ffc8cf3c55660dd691ece1661d25bb0589f11
boatengnana <boatengnana427@gmail.com>
2023-02-17 18:55:41 -0800

    low level programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main: Entry point 
+ *
+ * Always return 0 (Succes)
+ */
+
 
 int main(void)
 {
-
-	puts("Programming is like building a multilingual puzzle");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
