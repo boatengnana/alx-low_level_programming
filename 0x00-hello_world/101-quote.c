3d7bd362054bb18cdd2c11fd717bd39d986ddaac
boatengnana <boatengnana427@gmail.com>
2023-02-18 20:21:18 -0800

    quote of the day

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 25-10-19\n ")
+	return(0)
+}
