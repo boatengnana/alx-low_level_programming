0c66ec988ea440664cc7ef20e40fd24ddb54fe79
boatengnana <boatengnana427@gmail.com>
2023-02-20 21:19:40 -0800

    0x01

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 9; i++) 
+	for (i = 0; i < 9; i++)
 	{
-		for (j = i + 1; j <= 9; j++) 
+		for (j = i + 1; j <= 9; j++)
 		{
 			for (k = j + 1; k <= 9; k++)
 			{
@@ -19,7 +19,7 @@ int main(void)
 				putchar(j + '0');
 				putchar(k + '0');
 
-				if(i != 7 || j != 8 || k != 9)
+				if (i != 7 || j != 8 || k != 9)
 				{
 					putchar(',');
 					putchar(' ');
