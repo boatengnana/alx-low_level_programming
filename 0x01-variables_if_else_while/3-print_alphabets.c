0e78ad740cfdb26cb40d636cb90c756a823bcd07
boatengnana <boatengnana427@gmail.com>
2023-02-20 19:07:03 -0800

    0x001

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char uppercase = 'A';
+	char lowercase = 'a';
+
+	while (lowercase <= 'z')
+	{
+		putchar(lowercase);
+		lowercase++;
+	}
+
+	while (uppercase <= 'Z')
+	{
+		putchar(uppercase);
+		uppercase++;
+	}
+
+	putchar('\n');
+
+	return (0);
