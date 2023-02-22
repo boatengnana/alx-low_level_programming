73237feb1619fb4668b8c65d1ef5f206970cb9b0
boatengnana <boatengnana427@gmail.com>
2023-02-21 22:04:05 -0800

    functions and loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * alphabet - prints all alphabets in upper case
+ *
+ * Return: always 0 (Success)
+ */
+
+void alphabet(void)
+{
+	for (c = 'z'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	alphabet();
+	putchar('\n');
+	return(0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 
 
 /**
@@ -7,3 +8,4 @@
  */
 
 int _putchar(char c);
+void alphabet(void);
