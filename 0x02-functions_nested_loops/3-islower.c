854c6fd0bc869cf72694d708077f177dfe45e9bf
boatengnana <boatengnana427@gmail.com>
2023-02-21 22:31:48 -0800

    functions and loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - checks if a value is lower case then prints out the result
+ *
+ * Return: nothing
+ */
+int _islower(char c)
+{
+	int result = islower(c);
+	result? return 1: return 0;
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
-
+#include <ctype.h>
 
 /**
  * Function declarations
@@ -10,3 +10,4 @@
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(char c);
