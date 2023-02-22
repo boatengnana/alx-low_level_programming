4d1ec7dc3226acd2b14876c6bdc2558c3b81bced
boatengnana <boatengnana427@gmail.com>
2023-02-21 23:08:57 -0800

    functions and loops

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,7 +2,7 @@
 
 /**
  * _abs - resolves the absolute value of a value
- * n: the value to be turned absolute
+ * @n: the value to be turned absolute
  *
  * Return: the absolute value
  */
@@ -10,6 +10,6 @@ int _abs(int n)
 {
 	int abs_value;
 
-	abs_value = n < 0? -n: n;
+	abs_value = n < 0 ? -n : n;
 	return (abs_value);
 }
