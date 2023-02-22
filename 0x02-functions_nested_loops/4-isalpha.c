1b58b760d2193c0f330930e4a626758775f124e0
boatengnana <boatengnana427@gmail.com>
2023-02-21 22:48:14 -0800

    functions and loops

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**_isalpha - checks whether a character is an alphabet
+ * @c: the value to run the check on
+ *
+ * Return: 1 for true and 0 for otherwise
+ */
+int _isalpha(char c)
+{
+	int result = isalpha(c);
+	
+	if (result == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
