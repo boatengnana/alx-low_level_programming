f35dfa7cf174c48eabe7250feeaa20ff24b516b5
boatengnana <boatengnana427@gmail.com>
2023-02-21 21:47:46 -0800

    functions and loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,16 +3,6 @@
 #include <string.h>
 
 /**
- * _putchar - writes the character c to standard output
- * @c: the character to print
- *
- * Return: On success 1
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
