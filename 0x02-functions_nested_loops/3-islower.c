806be109a0d7a8af49c47dda740d65b098f36376
boatengnana <boatengnana427@gmail.com>
2023-02-21 22:41:32 -0800

    functions and loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,12 +2,14 @@
 
 /**
  * _islower - checks if a value is lower case then prints out the result
+ *@c is the value to be checked
  *
  * Return: nothing
  */
 int _islower(char c)
 {
 	int result = islower(c);
+
 	if (result == 0)
 	{
 		return (0);
