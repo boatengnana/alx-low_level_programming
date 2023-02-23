ba8869fdb9210efd9fcf2804cd5ead2e5292cec2
boatengnana <boatengnana427@gmail.com>
2023-02-22 20:28:26 -0800

    Low level programming

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,15 +9,18 @@ void times_table(void)
 {
 	int i, j;
 
-	for (i = 0; i <=9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 0; j <=9; j++)
+		for (j = 0; j <= 9; j++)
 		{
 			int mul = i * j;
 			if ( mul > 9 )
 			{
 				_putchar((mul / 10) + '0');
 				_putchar((mul % 10) + '0');
+			}else if (j == 0)
+			{
+				_putchar('0')
 			}
 			else
 			{
