a08707d4b5135d808abded72891ff5cb165591e6
boatengnana <boatengnana427@gmail.com>
2023-02-22 19:01:42 -0800

    Low level programming

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,18 +8,21 @@
 void jack_bauer(void)
 {
 	int m, h;
-	char zero, zerom;
 
 	for (h = 0; h < 24; h++)
 	{
 		for (m = 0; m < 60 m++)
 		{
-			zero = h > 9 ? '' : '0';
-			zerom = m > 9 ? '' : '0';
-			putchar('0' + zero);
+			if (h <= 9)
+			{
+				putchar('0');
+			}
 			putchar('0' + h);
 			putchar(':');
-			putchar('0' + zerom);
+			if (m <= 9)
+			{
+				putchar('0');
+			}
 			putchar('0' + m);
 		}
 	}
