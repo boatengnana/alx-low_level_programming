2a3bd31d7e4f745392cbcfdb0b21df21e6cb9600
boatengnana <boatengnana427@gmail.com>
2023-02-22 21:22:22 -0800

    Low level programming

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * print_times_table - print somehte adf
+ * @n: parameter
+ *
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	int y, m;
+
+	if (n > 15 || n < 0)
+	{
+		return;
+	}
+	for (y = 0; y <= n; y++)
+	{
+		for (m = 0; m <= n; m++)
+		{
+			int mul = y * m;
+
+			if (mul = 0)
+			{
+				_putchar('0');
+			}
+			else if(mul <=9)
+			{
+				_putchar((mul / 10) + '0');
+				_putchar((mul % 10) + '0');
+			}
+			else
+			{
+				_putchar((mul / 100) + '0');
+				_putchar(((mul / 10) % 10) + '0');
+				_putchar((mul % 10) + '10');
+			}
+			if (m < n)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+}
