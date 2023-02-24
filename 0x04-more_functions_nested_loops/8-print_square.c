04649086f433a6e6c5e208dfdfbd8ae8fbcef618
boatengnana <boatengnana427@gmail.com>
2023-02-23 20:23:54 -0800

    debugging

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,7 +9,7 @@
 
 void print_square(int size)
 {
-	int i = 0;
+	int j = 0;
 
 	if (size <= 0)
 	{
@@ -18,7 +18,7 @@ void print_square(int size)
 
 	while (j < size)
 	{
-		int j = 0;
+		int i = 0;
 
 		while (i < size)
 		{
