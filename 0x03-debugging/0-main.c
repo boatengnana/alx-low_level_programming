14a21cb5947fa2ea14273e638eaca52382c67cae
boatengnana <boatengnana427@gmail.com>
2023-02-23 17:49:17 -0800

    debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints whether a number is negative or not
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
