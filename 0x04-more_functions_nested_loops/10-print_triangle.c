1b74a3321c4ee5463c6ed874e07fece31a1c48c0
boatengnana <boatengnana427@gmail.com>
2023-02-23 21:07:59 -0800

    debugging

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - some # triangle
+ * @size: size of the triangle
+ *
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size - 1)
+		{
+			_putchar(' ');
+		}
+
+		for (k = 1; k < i; k++)
+		{
+			_putchar('#');
+		}
+	}
+}
