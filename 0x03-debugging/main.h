126c594b59b19fd1c32cf5731f1328bf7d06dd1a
boatengnana <boatengnana427@gmail.com>
2023-02-23 19:01:13 -0800

    debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,4 +3,4 @@
 /* Function declarations */
 
 int largest_number(int a, int b, int c);
-int print_remaining_days(int month, int day, int year);
+void print_remaining_days(int month, int day, int year);
