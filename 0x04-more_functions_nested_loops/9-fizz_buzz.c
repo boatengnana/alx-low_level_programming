d1ec241ad6422260d0fac8085ea837a5c9230749
boatengnana <boatengnana427@gmail.com>
2023-02-24 18:43:22 -0800

    even more functions

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - fizz buzz problem
@@ -13,7 +13,7 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
+		if (i % 15 == 0)
 		{
 			_putchar('F');
 			_putchar('i');
