053de1bf0fdc415921912336f49a7f77490eceb1
boatengnana <boatengnana427@gmail.com>
2023-02-28 20:12:36 -0800

    pointers, arrays, and strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,7 +13,7 @@ void puts_half(char *str)
 	int len = strlen(str);
 	int i = len % 2 != 0 ? (len - 1) / 2 : len / 2;
 
-	while (i < len - 1)
+	while (i + < len - 1)
 	{
 		printf("%c", *(str + i));
 		i++;
