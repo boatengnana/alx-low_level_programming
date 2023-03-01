886371befaf0abe122a10f354fb99cfbbcdffa79
boatengnana <boatengnana427@gmail.com>
2023-02-28 18:58:04 -0800

    pointers, arrays, and strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strlen - evaluates the length of a string
+ * @n: string
+ *
+ * Return: int
+ */
+
+int _strlen(char *n)
+{
+	return strlen(*n);
+}
