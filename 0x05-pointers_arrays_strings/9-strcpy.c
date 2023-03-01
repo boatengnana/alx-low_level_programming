7bba15fe1a35e7d40b405ee844666ac57da9fc6f
boatengnana <boatengnana427@gmail.com>
2023-02-28 20:40:28 -0800

    pointers, arrays, and strings

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcpy - copy strings
+ * @dest: first parameter
+ * @src: second parameter
+ *
+ * Return: char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	while (*src != '\0')
+	{
+		*dest++ = *src++;
+	}
+	*dest = '0';
+}
