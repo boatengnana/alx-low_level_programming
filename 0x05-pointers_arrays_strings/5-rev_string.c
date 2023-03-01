2a9273340cb48035efbe35b67b2bd5e0d35e6e7a
boatengnana <boatengnana427@gmail.com>
2023-02-28 19:39:23 -0800

    pointers, arrays, and strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @str: value to be reversed
+ *
+ * Return: void
+ */
+
+void rev_string(char *str)
+{
+	int len = strlen(str);
+	char *p1 = str, *p2 = str + len - 1;
+
+	while (*p2 > *p1)
+	{
+		char temp = *p1;
+		*p1 = *p2;
+		*p2 = temp;
+		p1++;
+		p2++;
+	}
+}
