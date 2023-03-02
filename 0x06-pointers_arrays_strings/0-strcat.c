84814f8accd79acaf9b240d9c4a59ae03b55094c
boatengnana <boatengnana427@gmail.com>
2023-03-02 14:57:00 -0800

    more pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @src: second string
+ * @dest: first string
+ *
+ * Return: char * (string)
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (dest + src);
+}
