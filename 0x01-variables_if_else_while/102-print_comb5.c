ed063eabd4eae3bddbfb3ed4ca93d3d7cb05461c
boatengnana <boatengnana427@gmail.com>
2023-03-02 09:28:19 -0800

    redirections

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,26 +5,24 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-	int i, j;
 
-	for (i = 0; i <= 9; i++)
-	{
-		for (j = 0; j <= 9; j++)
-		{
-			putchar(i + '0');
-			putchar(j + '0');
-			putchar(' ');
-			putchar(i + '0');
-			putchar((j + 1) + '0');
-			if ( i == 9 && j == 8 && j + 1 == 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+int main() {
+    int num1, num2;
+
+    for (num1 = 0; num1 <= 99; num1++) {
+        for (num2 = num1; num2 <= 99; num2++) {
+            putchar((num1 / 10) + '0');
+            putchar((num1 % 10) + '0');
+            putchar(' ');
+            putchar((num2 / 10) + '0');
+            putchar((num2 % 10) + '0');
+            if (num1 != 99 || num2 != 99) {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+
+    putchar('\n');
+    return (0);
 }
