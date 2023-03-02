0b3bdb9561844a876691159b21f29c4c0111c8ae
boatengnana <boatengnana427@gmail.com>
2023-03-02 09:55:11 -0800

    redirections

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,57 +10,28 @@ int print_to_98(int n)
 {
 	int i;
 
-	if (n <= 98)
+	n > 98? i = n: i = 0;
+	for (; i <= 98; i++)
 	{
-	
-		for (i = n; i <= 98; i++)
+		if (n > 99)
 		{
-			if (n > 99)
-			{
-				_putchar((i / 100) + '0');
-				_putchar(((i / 10) % 10) + '0');
-				_putchar((i % 10) + '0');
-			}
-			else if (n > 9 && n <= 99)
-			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-			}
-			else
-			{
-				_putchar(i + '0');
-			}
-			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar((i / 100) + '0');
+			_putchar(((i / 10) % 10) + '0');
+			_putchar((i % 10) + '0');
 		}
-	}
-	else
-	{
-		for (i = n; i >= 98; i--)
+		else if (n > 9 && n <= 99)
 		{
-			if (n > 99)
-			{
-				_putchar((i / 100) + '0');
-				_putchar(((i / 10) % 10) + '0');
-				_putchar((i % 10) + '0');
-			}
-			else if ((n > 9) && (n <= 99))
-			{
-				_putchar((i / 10) + '0');
-				_putchar((i % 10) + '0');
-			}
-			else
-			{
-				_putchar(i + '0');
-			}
-			if (i != 98)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		else
+		{
+			_putchar(i + '0');
+		}
+		if (i != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
 		}
 	}
 	putchar('\n');
