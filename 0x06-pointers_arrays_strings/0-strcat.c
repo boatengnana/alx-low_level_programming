9f620148c514602f8919dbcc27cec4816e8f0cb6
boatengnana <boatengnana427@gmail.com>
2023-03-02 19:26:18 -0800

    more pointers arrays and strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,6 +10,21 @@
 
 char *_strcat(char *dest, char *src)
 {
-	dest = dest + src;
-	return (*dest);
+	char *ptr = dest;
+
+	while (*ptr != '\0')
+	{
+		ptr++;
+	}
+
+	while (*src != '\0')
+	{
+		*ptr = *src;
+		ptr++;
+		src++;
+	}
+
+	*ptr = '\0';
+
+	return (0);
 }
