1a20845c6287de83c4341a710cd67c5393085c86
boatengnana <boatengnana427@gmail.com>
2023-03-05 14:41:26 -0800

    arrays, pointers, and strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,14 +9,18 @@
  * Return: char
  */
 
-char *_strncpy(char *dest, char *src, int n) {
+char *_strncpy(char *dest, char *src, int n)
+{
 	int i;
 
-	for (i = 0; i < n && src[i] != '\0'; i++) {
-		dest[i] = src[i];
+	for (i = 0; i < n ; i++)
+	{
+		*(dest + i) = *(src + i)
 	}
-	for ( ; i < n; i++) {
-		dest[i] = '\0';
+	for ( ; i < n; i++)
+	{
+		*(dest + i) = '\0';
 	}
-	return dest;
+
+	return (dest);
 }
