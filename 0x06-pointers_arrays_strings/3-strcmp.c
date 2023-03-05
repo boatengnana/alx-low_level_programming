9e7e4ba41e7dfc43ccb4546a9157b3036d0b6fe6
boatengnana <boatengnana427@gmail.com>
2023-03-05 15:05:08 -0800

    arrays, pointers, and strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: int
+ */
+
+int _strcmp(char* s1, chsr* s2)
+{
+	int *string1 = s1;
+	int *string2 = s2;
+
+	while (*string1 != *string2)
+	{
+		string1++;
+		string2++;
+	}
+
+	if (*string1 > *string2)
+	{
+		return (15);
+	}
+	else if (*string1 < *string2)
+	{
+		return (-15);
+	}
+	else
+	{
+		return (0);
+	}
+}
+			
