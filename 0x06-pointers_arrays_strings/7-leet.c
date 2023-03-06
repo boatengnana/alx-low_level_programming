bf45d724232ee188830ef2a0b2167ec6ece93d98
boatengnana <boatengnana427@gmail.com>
2023-03-05 17:21:43 -0800

    pointers, arrays, and strings

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,12 +10,13 @@
 char *leet(char *c)
 {
 	int i = 0;
-	int j = 0;
 	char leet_table[] = "4433007711";
 	char *letter = "aAeEoOtTlL";
 
 	while (*(c + i) != '\0')
 	{
+		int j = 0;
+
 		while (j < strlen(letter))
 		{
 			if (*(c + i) == letter[j])
