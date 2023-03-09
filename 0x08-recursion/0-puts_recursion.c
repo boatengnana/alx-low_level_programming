16d5f6fe0e816bc9081027a2909540e64aaf96e3
boatengnana <boatengnana427@gmail.com>
2023-03-08 21:01:50 -0800

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,13 +9,12 @@
 
 void _puts_recursion(char *s)
 {
-	while (*s != '\0')
+	if (*s == '\0')
 	{
-		_putchar(*(s));
-
-		s++;
-
-		_puts_recursion(s);
+		return;
 	}
 
+	_putchar(*s);
+
+	_puts_recursion(s + 1);
 }
