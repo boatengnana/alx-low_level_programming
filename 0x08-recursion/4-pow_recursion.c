75b0ed5ea261e6b52ad851db742e0727cc1191b6
boatengnana <boatengnana427@gmail.com>
2023-03-08 21:51:17 -0800

    recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -14,14 +14,13 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-
-	if (y > 0)
+	else if (y == 0)
 	{
-		return (x * _pow_recursion(x, y - 1));
+		return (1);
 	}
 	else
 	{
-		return (1);
+		return (x * _pow_recursion(x, y - 1));
 	}
 }
 
