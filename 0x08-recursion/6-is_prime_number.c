85168105ef3c332527ce511f944d8a02d2135e44
boatengnana <boatengnana427@gmail.com>
2023-03-08 20:27:28 -0800

    recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,22 +8,22 @@
  * Return: 1 if prime 0 if not
  */
 
-int is_prime_number(int n)
+int is_prime_number(int n, int i = 2)
 {
-	int i = 2;
+	if (i > sqrt(n))
+	{
+		return (1);
+	}
+
+	if (n % i == 0)
+	{
+		return (0);
+	}
 
 	if (n < 2)
 	{
 		return (0);
 	}
 
-	for (; i <= sqrt(n); i++)
-	{
-		if (n % i == 0)
-		{
-			return (0);
-		}
-	}
-
-	return (1);
+	return is_prime_number(n, i + 1);
 }
