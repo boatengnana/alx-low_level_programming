dec08d5bab6f5850b2aafe9c8d2c003f4aef1e87
boatengnana <boatengnana427@gmail.com>
2023-03-08 19:10:03 -0800

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string using recursion
+ * @s: first character in the string
+ *
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	while (s != '\0')
+	{
+		_putchar(*(s));
+
+		s++;
+
+		_puts_recursion(&s);
+	}
+
+}
