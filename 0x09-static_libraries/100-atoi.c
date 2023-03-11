2576bb716f370d5fd7f52b4cd42987dc6d0c17b2
boatengnana <boatengnana427@gmail.com>
2023-03-10 22:40:49 -0800

    fixed bugs

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -15,40 +15,5 @@ int _atoi(char *c)
 	int i = 0;
 	int prev_num = 0;
 
-	while (c[i] == ' ')
-	{
-		i++;
-	}
-
-	if (c[i] == '-')
-	{
-		sign = -1;
-		i++;
-	}
-	else if (c[i] == '+')
-	{
-		i++;
-	}
-
-	while (c[i] >= '0' && c[i] <= '9')
-	{
-		prev_num = num;
-		num = num * 10 + (c[i] - '0');
-
-		if (num < prev_num)
-		{
-			if (sign == 1)
-			{
-
-				return (INT_MAX);
-			}
-			else
-			{
-				return (INT_MIN);
-			}
-		}
-		i++;
-	}
-
 	return (sign * num);
 }
--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,5 +1,12 @@
 #include <unistd.h>
 
+/**
+ * _putchar - putchar
+ * @c: character
+ *
+ * Return: int
+ */
+
 int _putchar(char c)
 {
 	return (write(1, &c, 1));
