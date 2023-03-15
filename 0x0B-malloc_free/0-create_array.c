f0491ea3a53fe56b4af4ef62faa519d559ca1906
boatengnana <boatengnana427@gmail.com>
2023-03-15 16:27:41 -0700

    array created

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * create_array - creates and array
+ * @size: the size of the array
+ * @c: the character to fill the array with
+ *
+ * Return: a pointer
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	char *mem = malloc(size * sizeof(char));
+
+	if (*mem)
+	{
+		return (mem);
+	}
+	else
+	{
+		return (NULL);
+	}
+}
