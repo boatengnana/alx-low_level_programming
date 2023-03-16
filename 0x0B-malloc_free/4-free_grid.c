98535565c9cf24375b4e1df9951d9cc6a6d59433
boatengnana <boatengnana427@gmail.com>
2023-03-15 18:15:58 -0700

    malloc free

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/*
+ * free_grid - frees an 2D aray
+ * @grid: 2D array
+ * @height: rows
+ *
+ * Return: pointer to int pointer
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+
+	free(grid);
+}
