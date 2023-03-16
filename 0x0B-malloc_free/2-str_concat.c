fd7211e80de3ea77acd80b76a06c418822feadbb
boatengnana <boatengnana427@gmail.com>
2023-03-15 17:52:39 -0700

    malloc free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,12 +12,13 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *mem;
+	int len1 = 0;
+	int len2 = 0;
+	int i = 0;
+	int k = 0;
 
 	s1 = s1 == NULL ? "" : s1;
 	s2 = s2 == NULL ? "" : s2;
-
-	int len1 = 0, len2 = 0;
-	int i = 0, k = 0;
 
 	while (s1[len1] != '\0')
 	{
