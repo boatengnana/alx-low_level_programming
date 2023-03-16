34554462a20a8c98a57cd0681ea6a0f20f436b77
boatengnana <boatengnana427@gmail.com>
2023-03-15 18:39:30 -0700

    malloc free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,94 +1,60 @@
-#include "main.h"
-
 #include <stdlib.h>
-#include <stdio.h>
+#include <string.h>
 
-int count_words(char *);
-char **allocate_memory(int, char *);
-void free_memory(char **, int);
-int copy_word(char *, char *, int);
-
-char **strtow(char *str)
-{
-    char **words;
-    int num_words, i, j, k, len;
-
-    if (str == NULL || *str == '\0') {
+char **strtow(char *str) {
+    if (str == NULL || str[0] == '\0')
         return NULL;
+
+    int word_count = 0;
+    int i = 0;
+    while (str[i] != '\0') {
+        if (str[i] != ' ' && (i == 0 || str[i-1] == ' '))
+            word_count++;
+        i++;
     }
 
-    num_words = count_words(str);
-    words = allocate_memory(num_words, str);
-    if (words == NULL) {
+    char **words = malloc((word_count + 1) * sizeof(char *));
+    if (words == NULL)
         return NULL;
-    }
 
-    for (i = 0, j = 0; j < num_words; i++) {
-        if (str[i] != ' ' && str[i] != '\t' && str[i] != '\n') {
-            len = 0;
-            k = i;
-            while (str[k] != ' ' && str[k] != '\t' && str[k] != '\n' && str[k] != '\0') {
-                len++;
-                k++;
-            }
-            words[j] = malloc((len + 1) * sizeof(char));
+    int j = 0;
+    int word_start = 0;
+    for (i = 0; i < strlen(str); i++) {
+        if (str[i] != ' ' && (i == 0 || str[i-1] == ' ')) {
+            word_start = i;
+        }
+        else if (str[i] == ' ' && i > 0 && str[i-1] != ' ') {
+            int word_len = i - word_start;
+            words[j] = malloc((word_len + 1) * sizeof(char));
             if (words[j] == NULL) {
-                free_memory(words, j);
+                free_words(words, j);
                 return NULL;
             }
-            copy_word(words[j], str + i, len);
+            strncpy(words[j], &str[word_start], word_len);
+            words[j][word_len] = '\0';
             j++;
-            i = k;
         }
+    }
+
+    if (i > 0 && str[i-1] != ' ') {
+        int word_len = i - word_start;
+        words[j] = malloc((word_len + 1) * sizeof(char));
+        if (words[j] == NULL) {
+            free_words(words, j);
+            return NULL;
+        }
+        strncpy(words[j], &str[word_start], word_len);
+        words[j][word_len] = '\0';
+        j++;
     }
 
     words[j] = NULL;
     return words;
 }
 
-int count_words(char *str)
-{
-    int count = 0, i;
-
-    for (i = 0; str[i] != '\0'; i++) {
-        if ((i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n') && str[i] != ' ' && str[i] != '\t' && str[i] != '\n') {
-            count++;
-        }
-    }
-
-    return count;
-}
-
-char **allocate_memory(int num_words, char *str)
-{
-    char **words;
-
-    words = malloc((num_words + 1) * sizeof(char *));
-    if (words == NULL) {
-        return NULL;
-    }
-
-    return words;
-}
-
-void free_memory(char **words, int num_words)
-{
-    int i;
-
-    for (i = 0; i < num_words; i++) {
+void free_words(char **words, int count) {
+    for (int i = 0; i < count; i++) {
         free(words[i]);
     }
     free(words);
-}
-
-int copy_word(char *word, char *str, int len)
-{
-    int i;
-
-    for (i = 0; i < len; i++) {
-        word[i] = str[i];
-    }
-    word[i] = '\0';
-
-    return 0;
 }
