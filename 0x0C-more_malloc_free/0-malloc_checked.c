4a7c942530dd55b08e1f4fd99f06ebf0cfaea99d
boatengnana <boatengnana427@gmail.com>
2023-03-16 16:54:43 -0700

    more malloc free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * malloc_checked - returns an empty memory location
+ * @b: determinant for the size of space
+ *
+ * Return: Nothing
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *mem;
+
+	*mem = malloc(b);
+
+	if (mem == NULL)
+	{
+		exit(98);
+	}
+
+}
