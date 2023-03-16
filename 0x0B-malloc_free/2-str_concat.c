919623d597565fc9df885b61d4c936fff5391500
boatengnana <boatengnana427@gmail.com>
2023-03-15 17:38:44 -0700

    malloc free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,7 +14,7 @@ char *str_concat(char *s1, char *s2)
 	int s1_len = strlen(s1);
 	int s2_len = strlen(s2);
 	char *mem;
-
+	
 	mem = malloc(sizeof(char) * (s1_len + s2_len + 1));
 
 	if (mem == NULL)
