fd479a23ca51b57eb2a46664c8fe83742b0b7b47
boatengnana <boatengnana427@gmail.com>
2023-03-16 19:01:38 -0700

    more malloc free

---
+++ 0x0C-more_malloc_free/3-array_arrange.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * array_range - creates an array from a min to a max value
+ * @min: min value
+ * @max: max value
+ *
+ * Return: pointer to an integer
+ */
+
+int *array_range(int min, int max)
+{
+	void *mem;
+	int i = 0;
+
+	if (min > max)
+		return (NULL);
+
+	mem = malloc(sizeof(min) * (max - min + 1));
+
+	if (mem == NULL)
+		return (NULL);
+
+	while (i < (max - min + 1))
+	{
+		mem[i] = min + i;
+		i++;
+	}
+
+	return (mem);
+}
