73d43e24ab537e748fb8f733d828cb51ee9e4a2b
boatengnana <boatengnana427@gmail.com>
2023-03-16 17:50:21 -0700

    more malloc free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,19 +12,13 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
 	char *mem;
-	unsigned int len2 = 0;
-	unsigned int len1 = 0;
-	unsigned int i = 0, k = 0;
+	unsigned int len2 = 0, len1 = 0, i = 0, k = 0;
 
 	while (s1[len1] != '\0')
 		len1++;
@@ -37,19 +31,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	mem = malloc(len1 + limit + 1);
 
 	if (mem == NULL)
-	{
 		return (NULL);
-	}
 
 	for (; i < len1; i++)
-	{
 		mem[i] = s1[i];
-	}
 
 	for (; k < limit; k++)
-	{
 		mem[len1 + k] = s2[k];
-	}
 
 	mem[len1 + limit] = '\0';
 
